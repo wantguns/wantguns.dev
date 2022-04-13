f0426c717fcc80fa9d17dea7d7d190cc873413bd
Gunwant Jain <mail@wantguns.dev>
2022-04-14 00:28:32 +0530

    footnotes: add backlinks
    copypasta from github.com/getzola/zola/issues/1285
    
    Signed-off-by: Gunwant Jain <mail@wantguns.dev>

--- static/js/page.js
+++ static/js/page.js
@@ -4,6 +4,7 @@ window.onload = function () {
     toc_scroll_position(container);
     window.onscroll = function () { toc_scroll_position(container) };
   }
+  addBackLinks();
 }
 
 function toc_scroll_position(container) {
@@ -39,3 +40,26 @@ function toc_scroll_position(container) {
     current_toc_item.classList.add("active");
   }
 }
+
+// This adds links back to where a footnote is originally referenced.
+const addBackLinks = () => {
+  for (const ref of document.getElementsByClassName('footnote-reference')) {
+    const hash = ref.children[0].hash.substring(1);
+    const refhash = 'ref:' + hash;
+    ref.id = refhash;
+  }
+
+  for (const footnote of document.getElementsByClassName('footnote-definition')) {
+    const hash = footnote.id;
+    const refhash = 'ref:' + hash;
+    const backlink = document.createElement('a');
+
+    backlink.href = '#' + refhash;
+    backlink.className = 'footnote-backlink';
+    backlink.innerText = '↩︎';
+
+    sup = footnote.children[1];
+    sup.appendChild(backlink);
+  }
+}
+
